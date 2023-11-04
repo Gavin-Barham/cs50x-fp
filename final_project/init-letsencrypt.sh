68b7b2a71674b22e5dec04065d6c6822ae7c7115
Gavin Barham <gavinbarham@yahoo.com>
2023-11-04 17:17:38 -0500

    fix url for currernt site

--- final_project/init-letsencrypt.sh
+++ final_project/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=("health-tracker-api.com" "www.health-tracker-api.com")
+domains=("freakyfastdelivery.com" "www.freakyfastdelivery.com")
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gavinbarham@yahoo.com" # Adding a valid address is strongly recommended
